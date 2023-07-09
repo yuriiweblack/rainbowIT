bc40acef94d3911fb02b7fefcc4350ec68127f88
yuriiweblack <yuriiwebblack@gmail.com>
2023-07-09 16:40:15 +0300

    about with adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(function () {
 
  $('.menu__btn').on('click', function () {
-
-  // $('.menu__list').toggleClass('menu__list--active');
   $('.menu__list').toggleClass('menu__list--activ');
  })
 
